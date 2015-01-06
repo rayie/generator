718e51b56cc6ed18376d091df577a35d57532c5f
Ray Ie <ie.raymond@gmail.com>
2015-01-05 22:44:31 -0800

    added script and css referencing in layout for angular, bootstrap, and angular-ui

--- templates/js/app.js
+++ templates/js/app.js
@@ -26,8 +26,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());{css}
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 app.use(serveStatic(path.join(__dirname, 'public')));
+app.use(serveStatic(path.join(__dirname, 'bower_components')));
 app.use(session({ store: new RedisStore(), secret: 'somekindofmagic' }));
 
 
